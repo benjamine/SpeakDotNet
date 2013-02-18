7faacb8268ccf2946c5cdaf173a88f730ea03297
Benjamin Eidelman <beneidel@gmail.com>
2013-02-18 15:14:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+SpeakDotNet
+=============
+
+Simple .net console tool for Text-to-Speech using Windows Speech API.
+
+
+Usage
+------
+
+```
+	SpeakDotNet "Say this thru the default audio device"
+	SpeakDotNet "Save this speech in a file" audio-file.wav
+```
+
+Note: output format for .wav file is PCM 16bit, 16Khz, Mono
---
+++ SpeakDotNet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = args.Length > 0 ? args[0] : "enter some text as first argument to this command";
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.
+            if (args.Length > 1)
+            {
+                var audioformat = new SpeechAudioFormatInfo(16000,AudioBitsPerSample.Sixteen, AudioChannel.Mono);
+                synth.SetOutputToWaveFile(args[1], audioformat);
+            }
+            else
+            {
+                synth.SetOutputToDefaultAudioDevice();
+            }
+
+            // Speak a string.
+            synth.Speak(text);
+        }
+    }
+}
